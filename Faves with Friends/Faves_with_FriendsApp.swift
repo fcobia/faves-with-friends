41e29d33f0764039a909736c77f001c5d289e5ab
Frank Cobia <frank_cobia@me.com>
2021-11-24 22:35:19 -0500

    Initial Commit

---
+++ Faves with Friends/Faves_with_FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Faves_with_FriendsApp.swift
+//  Faves with Friends
+//
+//  Created by Frank Cobia on 11/24/21.
+//
+
+import SwiftUI
+
+@main
+struct Faves_with_FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Faves with FriendsUITests/Faves_with_FriendsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Faves_with_FriendsUITestsLaunchTests.swift
+//  Faves with FriendsUITests
+//
+//  Created by Frank Cobia on 11/24/21.
+//
+
+import XCTest
+
+class Faves_with_FriendsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
