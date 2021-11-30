60a16db631b27ac2437b5cbc6996557885727c52
Chris Riner <chrisriner@comcast.net>
2021-11-30 15:43:29 -0500

    working more on movie detail screen...

--- Faves with Friends/Screens/Movie/MovieDetailScreenView.swift
+++ Faves with Friends/Screens/Movie/MovieDetailScreenView.swift
@@ -20,40 +20,90 @@ struct MovieDetailScreenView: View {
     @State private var movie: Movie? = nil
     
     var body: some View {
-        NavigationView {
-            Form {
-                if let movie = movie {
-                    AsyncImage(url: movie.backdropPath) { phase in
-                        switch phase {
-                        case .empty:
-                            ZStack {
-                                Rectangle()
-                                    .fill(Color.clear)
-                                    .frame(width: 100, height: 100)
-                                ProgressView()
+        VStack {
+            if let movie = movie {
+                ZStack(alignment: .top) {
+                    VStack(alignment: .leading) {
+                        ZStack(alignment: .topLeading) {
+                            AsyncImage(url: movie.backdropPath) { phase in
+                                switch phase {
+                                case .empty:
+                                    ZStack {
+                                        Rectangle()
+                                            .fill(Color.clear)
+                                            .frame(width: 100, height: 100)
+                                        ProgressView()
+                                    }
+                                case .success(let image):
+                                    image.resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(maxWidth: .infinity, maxHeight: 300)
+                                        .ignoresSafeArea()
+                                        .overlay {
+                                            Rectangle()
+                                                .fill(Color.gray)
+                                                .opacity(0.50)
+                                                .frame(maxWidth: .infinity, maxHeight: 300)
+                                                .ignoresSafeArea()
+                                        }
+                                case .failure:
+                                    Image(systemName: "photo")
+                                        .frame(width: .infinity, height: 300)
+                                @unknown default:
+                                    // Since the AsyncImagePhase enum isn't frozen,
+                                    // we need to add this currently unused fallback
+                                    // to handle any new cases that might be added
+                                    // in the future:
+                                    EmptyView()
+                                }
                             }
-                        case .success(let image):
-                            image.resizable()
-                                .aspectRatio(contentMode: .fit)
-                                 .frame(maxWidth: .infinity, maxHeight: 300)
-                        case .failure:
-                            Image(systemName: "photo")
-                                .frame(width: .infinity, height: 300)
-                        @unknown default:
-                            // Since the AsyncImagePhase enum isn't frozen,
-                            // we need to add this currently unused fallback
-                            // to handle any new cases that might be added
-                            // in the future:
-                            EmptyView()
+                            VStack(alignment: .leading) {
+                                HStack {
+                                    AsyncImage(url: movie.posterPath) { phase in
+                                        switch phase {
+                                        case .empty:
+                                            ZStack {
+                                                Rectangle()
+                                                    .fill(Color.clear)
+                                                    .frame(width: 100, height: 100)
+                                                ProgressView()
+                                            }
+                                        case .success(let image):
+                                            image.resizable()
+                                                .aspectRatio(contentMode: .fit)
+                                                .frame(maxWidth: 100, maxHeight: 100)
+                                        case .failure:
+                                            Image(systemName: "photo")
+                                                .frame(width: 100, height: 100)
+                                        @unknown default:
+                                            // Since the AsyncImagePhase enum isn't frozen,
+                                            // we need to add this currently unused fallback
+                                            // to handle any new cases that might be added
+                                            // in the future:
+                                            EmptyView()
+                                        }
+                                    }
+                                    VStack(alignment: .leading) {
+                                        Text(movie.title)
+                                            .foregroundColor(.white)
+                                        Text(movie.releaseDate)
+                                            .font(.footnote)
+                                            .foregroundColor(.white)
+                                    }
+                                }
+                            }
                         }
                     }
-                } else {
-                    ProgressView()
                 }
+            } else {
+                ProgressView()
             }
-            .onAppear() {
-                getMovieDetails(id: id)
+            Form {
+                Text("Form test")
             }
+        }
+        .onAppear() {
+            getMovieDetails(id: id)
         }
     }
     
@@ -78,3 +128,4 @@ struct MovieDetailScreenView: View {
 //        MovieDetailScreenView()
 //    }
 //}
+
